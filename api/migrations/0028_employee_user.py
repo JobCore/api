f876d8fdaf281e67f1e8167e039ff205e1ca9629
Alejandro Sanchez <aalejo@gmail.com>
2018-09-17 17:38:49 +0000

    moved the user object

--- api/migrations/0028_employee_user.py
+++ api/migrations/0028_employee_user.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='employee',
             name='user',
-            field=models.OneToOneField(blank=True, default=None, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.OneToOneField(blank=True, null=True, default=None, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
