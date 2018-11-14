ec4c8c75574a709d64f1fc46e1e0719136d278ed
Alejandro Sanchez <aalejo@gmail.com>
2018-11-14 19:22:10 +0000

    indent on the settings.yp

--- jobcore/settings.py
+++ jobcore/settings.py
@@ -139,8 +139,8 @@ STATICFILES_FINDERS = [
 ]
 
 # if(os.environ.get('DEBUG') != 'TRUE'):
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # else:
     # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
     
