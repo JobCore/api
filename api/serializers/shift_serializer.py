074b49f703700c86f3cb69a2af4e0d3f00bd7f23
Alejandro Sanchez <aalejo@gmail.com>
2019-01-11 20:54:00 +0000

    fix bug in employee/ratings

--- api/serializers/shift_serializer.py
+++ api/serializers/shift_serializer.py
@@ -219,7 +219,7 @@ class ShiftInviteSerializer(serializers.ModelSerializer):
         # if current_user.profile.employer == None:
         #     raise serializers.ValidationError('Only talents can invite talents')
                 
-        employees = ShiftEmployee.objects.filter(shift=data["invite"].shift, employee=data['employee'])
+        employees = ShiftEmployee.objects.filter(shift__id=data["invite"].shift.id, employee__id=data['employee'].id)
         if(len(employees) > 0):
             raise serializers.ValidationError('This talent is already working on this shift')
         
