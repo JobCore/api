eb06cca313a321b28034369fb79caa4325e22471
Angel Lacret <alacret@gmail.com>
2019-11-21 10:17:34 -0500

    adding city as manageable

--- jobcore/settings.py
+++ jobcore/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG') == 'TRUE')
+_ENV_DEBUG = os.environ.get('DEBUG')
+DEBUG = (_ENV_DEBUG == 'TRUE' or _ENV_DEBUG == 'True')
 ENVIRONMENT = os.environ.get('ENVIRONMENT')
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
