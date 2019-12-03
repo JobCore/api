57d0fbc1122dfe7c9cd6c6199e95b081ce9d47fd
Angel Lacret <alacret@gmail.com>
2019-12-03 14:44:30 -0500

    manage documents

--- api/admin.py
+++ api/admin.py
@@ -4,26 +4,37 @@ from .models import *
 # Register your models here.
 admin.site.register(Employer)
 
+
 class EmployeeAdmin(admin.ModelAdmin):
     search_fields = ('user__first_name', 'user__last_name', 'user__email')
     list_display = ('id', 'get_name', 'get_email', 'get_status')
     list_filter = ('user__profile__status',)
     list_per_page = 100
+
     def get_name(self, obj):
         return obj.user.first_name + ' ' + obj.user.last_name
+
     def get_email(self, obj):
         return obj.user.email
+
     def get_status(self, obj):
         return obj.user.profile.status
+
+
 admin.site.register(Employee, EmployeeAdmin)
 
 admin.site.register(Shift)
 
+
 class ShiftInviteAdmin(admin.ModelAdmin):
     list_display = ('id', 'shift', 'employee', 'status')
-    search_fields = ('employee__user__first_name', 'employee__user__last_name', 'employee__user__email', 'shift__position__title', 'shift__venue__title')
+    search_fields = (
+        'employee__user__first_name', 'employee__user__last_name', 'employee__user__email', 'shift__position__title',
+        'shift__venue__title')
     list_filter = ('status',)
     list_per_page = 100
+
+
 admin.site.register(ShiftInvite, ShiftInviteAdmin)
 
 admin.site.register(Profile)
@@ -41,12 +52,14 @@ admin.site.register(City)
 class ClockinAdmin(admin.ModelAdmin):
     list_display = ('id', 'employee', 'started_at', 'ended_at', 'shift')
     list_per_page = 100
+
+
 admin.site.register(Clockin, ClockinAdmin)
 
 admin.site.register(UserToken)
 admin.site.register(Notification)
 admin.site.register(JobCoreInvite)
 admin.site.register(BankAccount)
-#admin.site.register(Document)
+admin.site.register(EmployeeDocument)
 admin.site.register(PaymentDeduction)
 # admin.site.register(City)
