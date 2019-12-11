1f09c76e72b8627fd274da111efb662401f2f898
Alejandro Sanchez <aalejo@gmail.com>
2019-12-11 19:38:33 +0000

    fixing registration test

--- api/tests/public_views/test_registration.py
+++ api/tests/public_views/test_registration.py
@@ -210,7 +210,7 @@ class RegistrationTestSuite(TestCase, WithMakeUser):
             'first_name': 'Alpha',
             'last_name': 'Bravo',
             'email': 'delta@mail.tld',
-            'password': 'AadsasdadsdasBD',
+            'password': 'AadadsdasBD',
             'account_type': 'employer',
             'employer': self.employer.id,
         }
