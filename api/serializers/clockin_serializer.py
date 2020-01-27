ef4754c164a66695ed0e57d19a3de7ab202e53b8
Alejandro Sanchez <aalejo@gmail.com>
2020-01-27 20:54:46 +0000

    distans in miles tst

--- api/serializers/clockin_serializer.py
+++ api/serializers/clockin_serializer.py
@@ -157,7 +157,7 @@ class ClockinSerializer(serializers.ModelSerializer):
         clockin = super().update(clockin, validated_data)
 
         shift_lat, shift_lon = [clockin.shift.venue.latitude, clockin.shift.venue.longitude]
-        print("Test... "+str(clockin.distance_in_miles))
+        print("Test... ")
         if 'latitude_in' in validated_data:
             clockin.distance_in_miles = haversine(validated_data['latitude_in'], validated_data['longitude_in'], shift_lat, shift_lon)
             print("Calculated distance: "+str(clockin.distance_in_miles))
