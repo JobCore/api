2e70eb725ea70d053996021a066e70357e65bacd
Luis Llave <llaveluis@gmail.com>
2020-01-27 20:17:59 -0500

    Add definition of admin interface for PreDefinedDeduction model

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-admin.site.register(Employer)
+
+class AppVersionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'version', 'force_update', 'created_at', 'updated_at')
+
+
+class ClockinAdmin(admin.ModelAdmin):
+    list_display = ('id', 'employee', 'started_at', 'ended_at', 'shift', 'author')
+    search_fields = (
+        'employee__user__first_name', 'employee__user__last_name', 'employee__user__email', 'author__user__first_name',
+        'author__user__last_name')
+    list_filter = ('status',)
+    list_per_page = 100
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'validates_identity', 'validates_employment', 'is_form')
 
 
 class EmployeeAdmin(admin.ModelAdmin):
@@ -21,9 +35,20 @@ class EmployeeAdmin(admin.ModelAdmin):
         return obj.user.profile.status
 
 
-admin.site.register(Employee, EmployeeAdmin)
+class EmployeeDocumentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'document', 'get_name', 'status', 'created_at', 'updated_at')
+    search_fields = (
+        'state', 'name', 'employee__user__first_name', 'employee__user__last_name', 'employee__user__email')
+    list_filter = ('status','document_type__validates_identity','document_type__validates_employment','document_type__is_form')
+    list_per_page = 100
 
-admin.site.register(Shift)
+    def get_name(self, obj):
+        return obj.document_type.title if obj.document_type is not None else 'Missing document type'
+
+
+class PreDefinedDeductionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'type', 'value', )
+    ordering = ('id', )
 
 
 class ShiftInviteAdmin(admin.ModelAdmin):
@@ -35,55 +60,27 @@ class ShiftInviteAdmin(admin.ModelAdmin):
     list_per_page = 100
 
 
-admin.site.register(ShiftInvite, ShiftInviteAdmin)
-
-admin.site.register(Profile)
+admin.site.register(AppVersion, AppVersionAdmin)
 admin.site.register(Badge)
-admin.site.register(Position)
-admin.site.register(Venue)
+admin.site.register(BankAccount)
+admin.site.register(Clockin, ClockinAdmin)
+admin.site.register(City)
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(EmployeeDocument, EmployeeDocumentAdmin)
+admin.site.register(Employer)
 admin.site.register(FavoriteList)
 admin.site.register(FCMDevice)
-admin.site.register(Rate)
-admin.site.register(PayrollPeriodPayment)
-admin.site.register(PayrollPeriod)
-admin.site.register(City)
-
-
-class ClockinAdmin(admin.ModelAdmin):
-    list_display = ('id', 'employee', 'started_at', 'ended_at', 'shift', 'author')
-    search_fields = (
-        'employee__user__first_name', 'employee__user__last_name', 'employee__user__email', 'author__user__first_name',
-        'author__user__last_name')
-    list_filter = ('status',)
-    list_per_page = 100
-
-
-class EmployeeDocumentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'document', 'get_name', 'status', 'created_at', 'updated_at')
-    search_fields = (
-        'state', 'name', 'employee__user__first_name', 'employee__user__last_name', 'employee__user__email')
-    list_filter = ('status','document_type__validates_identity','document_type__validates_employment','document_type__is_form')
-    list_per_page = 100
-
-    def get_name(self, obj):
-        return obj.document_type.title if obj.document_type is not None else 'Missing document type'
-admin.site.register(EmployeeDocument, EmployeeDocumentAdmin)
-class DocumentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'validates_identity', 'validates_employment', 'is_form')
-admin.site.register(Document, DocumentAdmin)
-
-
-
-
-admin.site.register(Clockin, ClockinAdmin)
-
-admin.site.register(UserToken)
-admin.site.register(Notification)
 admin.site.register(JobCoreInvite)
-admin.site.register(BankAccount)
-
-class AppVersionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'version', 'force_update', 'created_at', 'updated_at')
-admin.site.register(AppVersion, AppVersionAdmin)
-
+admin.site.register(Notification)
 admin.site.register(PaymentDeduction)
+admin.site.register(PayrollPeriod)
+admin.site.register(PayrollPeriodPayment)
+admin.site.register(Position)
+admin.site.register(PreDefinedDeduction, PreDefinedDeductionAdmin)
+admin.site.register(Profile)
+admin.site.register(Rate)
+admin.site.register(Shift)
+admin.site.register(ShiftInvite, ShiftInviteAdmin)
+admin.site.register(UserToken)
+admin.site.register(Venue)
