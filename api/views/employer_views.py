6d17bdd497c1415dd7ea3b8aee61ebf75bb154c3
Ixax <ixaxtav@gmail.com>
2021-09-02 18:01:33 +0000

    syntax error fix

--- api/views/employer_views.py
+++ api/views/employer_views.py
@@ -981,7 +981,7 @@ class EmployerMePayrollPeriodsView(EmployerView):
             defaultSerializer = payment_serializer.PayrollPeriodGetTinySerializer
             
             qPayments = request.GET.get('payments')
-            if qPayments is not None and qPayments != ''::
+            if qPayments is not None and qPayments != '':
                 defaultSerializer = payment_serializer.PayrollPeriodGetSmallSerializer
             
             serializer = defaultSerializer(periods.order_by('-starting_at'), many=True)
